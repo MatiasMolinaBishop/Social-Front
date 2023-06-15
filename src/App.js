d7c947a7d966f1dfec26892fcd8da97870a7ce85
Matias Molina Bishop <molibi01@gmail.com>
2023-06-15 19:47:02 +0200

    profile page first advances

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from './pages/Home';
+import Profile from './pages/profile/Profile';
 
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      <Profile/>
     </>
   );
 }
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,10 +1,42 @@
-import 'Profile.css'
+import './Profile.css'
+
+import Topbar from "../../components/topbar/Topbar"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Feed from "../../components/feed/Feed"
+import Rightbar from "../../components/rightbar/Rightbar"
 
 const Profile = () => {
     return(
-        <div>
-
+        <>
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">
+            <div className="profileCover">
+              <img
+                className="profileCoverImg"
+                src="assets/post/3.jpeg"
+                alt=""
+              />
+              <img
+                className="profileUserImg"
+                src="assets/person/7.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="profileInfo">
+                <h4 className="profileInfoName">Matias Molina</h4>
+                <span className="profileInfoDesc">I need to leanr and get a job!</span>
+            </div>
+          </div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar profile/>
+          </div>
         </div>
+      </div>
+    </>
     )
 }
 
